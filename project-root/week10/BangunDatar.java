fdd592a10eb34a5c3081d805d123b39d8ce95bf8
IlhamAhmadi <128119869+IlhamAhmadi@users.noreply.github.com>
2024-06-05 20:55:20 +0700

    Add files via upload

---
+++ project-root/week10/BangunDatar.java
@@ -0,0 +1,19 @@
+//IlhamAhmadi
+//A11.2022.14460
+
+package week10;
+
+public abstract class BangunDatar {
+
+    // Metode abstrak untuk menghitung luas tanpa parameter
+    public abstract double luas();
+
+    // Metode abstrak untuk menghitung luas dengan parameter
+    public abstract double luas(int a);
+
+    // Metode abstrak untuk menghitung keliling tanpa parameter
+    public abstract double keliling();
+
+    // Metode abstrak untuk menghitung keliling dengan parameter
+    public abstract double keliling(int a);
+}
